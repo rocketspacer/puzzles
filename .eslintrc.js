449c80eab6a44af60c221f8f5783ed280e54ce5e
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-04-25 21:54:50 +0700

    Add mocha and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "env": {
+        "browser": true,
+        "node": true,
+        "mocha": true
+    },
+    "rules": {
+        "comma-dangle": 0,
+        "no-bitwise": 0,
+        "camelcase": 0,
+        "func-names": 0,
+        "global-require": 0,
+        "prefer-arrow-callback": 0
+    }
+};
